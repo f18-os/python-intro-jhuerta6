02d1b36c0dab061464b4a6a7f862572b92617aea
jhuerta6 <jorgehuertahu@gmail.com>
2018-09-03 14:15:18 -0600

    Modified wordCountTest.py because of my PATH not finding python3 command. wordCount.py finished.

--- wordCount.py
+++ wordCount.py
@@ -7,29 +7,28 @@
 
 # Author: Jorge Huerta H.
 
-# To-do: create output file, sys.argv conditions
+# To-do: All done!
 
 import sys        # command line arguments
 import re         # regular expression tools
-import os         # checking if file exists
-import subprocess  # executing program
 
-if len(sys.argv) is not 2:
+if len(sys.argv) is not 3:
     print("Correct usage: wordCount.py <input text file> <output file>")
-    #exit()
+    exit()
 
 textFname = sys.argv[1]
 outputFname = sys.argv[2]
 words_master_list = []  # list of words
 dictionary = {}  # stores words and instances
 
-
 with open(textFname, 'r') as inputFile:
     for line in inputFile:
         line = line.strip()  # get rid of newline characters
         line = line.lower()  # line to lower case
         line = re.sub('[--]', ' ', line)  # remove punctuation
-        line = re.sub(r'[,.:;]', '', line)  # remove punctuation
+        #line = re.sub(r'[,.:;]', '', line)  # NOTE: Had to remove this to pass wordCountTest.py
+        # \s any whitespace character, \w any alphanumeric character
+        line = re.sub(r'[^\s\w]', ' ', line)  # remove NOT common word characters. NOTE: Added this to pass wordCountTest.py
         words_master_list.extend(line.split())  # extend our list of words
 
 for word in words_master_list:
@@ -40,8 +39,13 @@ for word in words_master_list:
     else:  # add word to dictionary
         dictionary[word] = 1  # populate an instance
 
-sorted_list = sorted(dictionary)
+sorted_list = sorted(dictionary)  # sort list for correct formatting
+
+f = open(outputFname, 'w')  # open output file to write to it
 
 for word in sorted_list:
-    instances = dictionary[word]
-    print(word + " " + str(instances))
+    instances = dictionary[word]  # number of instances of the word
+    f.write(word + " " + str(instances) + "\n")  # write to file
+
+f.close()  # close the file
+exit()  # exit program
--- wordCountTest.py
+++ wordCountTest.py
@@ -24,8 +24,9 @@ if not os.path.exists(textFname):
     print ("text file input %s doesn't exist! Exiting" % textFname)
     exit()
     
-#execute the program with 
-subprocess.call(["python3", "./wordCount.py", textFname, outputFname])
+#execute the program with
+# had to modify "python3" to "python", due to PATH settings
+subprocess.call(["python", "./wordCount.py", textFname, outputFname])
 
 #make sure output file exists
 if not os.path.exists(outputFname):
